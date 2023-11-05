38ea0898943dfb3b7b3d7e01adda32a28c172bc2
Adam <adam.crossland@zero1.co.uk>
2023-11-05 11:26:36 +0000

    updates

--- swagger-initializer.js
+++ swagger-initializer.js
@@ -3,7 +3,7 @@ window.onload = function() {
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: "https://petstore.swagger.io/v2/swagger.json",
+    url: "https://api-docs.mdoq.io/mdoq.yml",
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
